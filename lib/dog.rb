f00e549348d931e9990a4d75e9c54f0353e4b579
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-11 03:34:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,7 +54,7 @@ class Dog
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
     result = DB[:conn].execute(sql, id)[0]
-    Song.new(result[0], result[1], result[2])
+  #  Song.new(result[0], result[1], result[2])
   end 
   
   def self.find_or_create_by(name:, breed:)
