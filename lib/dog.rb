86227aacb05e44c5776c3345ca80b4e0efce2b6c
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-11 03:31:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -48,7 +48,10 @@ class Dog
   end 
   
   def self.new_from_db(row)
-    self.new(id: row[0], name: row[1], breed: row[2])
+    new_dog = self.new
+      new_dog.id = row[0]
+      new_dog.name = row[1] 
+      new_dog.breed = row[2]
   end 
   
   def self.find_by_id(id)
